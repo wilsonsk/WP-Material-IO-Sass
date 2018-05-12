72f450a117994d7a80f4d2398c6d40bdee00d755
Sky <wilson.skylerm@gmail.com>
2018-05-12 08:15:36 -0700

    0

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -8,7 +8,7 @@
 get_header();
 ?>
 
- <div class="container" style="position:relative;">
+ <div class="container">
     <!-- Sidebar Holder -->
     <nav id="sidebar">
       <div class="sidebar-header">
