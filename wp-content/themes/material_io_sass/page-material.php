4163ed931089e511e6800cb58725640220a4686a
Sky <wilson.skylerm@gmail.com>
2018-05-12 09:18:59 -0700

    0

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -10,12 +10,12 @@ get_header();
 
  <div class="container">
     <!-- Sidebar Holder -->
-    <nav id="sidebar">
-      <div class="sidebar-header">
+    <nav id="ui__sidebar">
+      <div class="ui__sidebar-header">
         <h3>Header as you want </h3>
         </h3>
       </div>
-      <ul class="list-unstyled components">
+      <ul class="list-unstyled ui__sidebar-components">
         <p>Dummy Heading</p>
         <li class="active">
         </li>
@@ -38,22 +38,8 @@ get_header();
       </ul>
     </nav>
 
-    <div id="ui-content">
-      <nav class="navbar navbar-default">
-        <div class="container">
-          <div class="navbar-header">
-            <button type="button" id="sidebarCollapse" class="btn btn-info navbar-btn">
-              <i class="glyphicon glyphicon-align-left"></i>
-              <span>Toggle Sidebar</span>
-            </button>
-          </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="#">Page</a></li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+    <div id="ui__content">
+
     </div>
   </div>
 
