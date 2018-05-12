d9314e2e2b793c95329584b32ed880f86e740f07
Sky <wilson.skylerm@gmail.com>
2018-05-11 22:54:03 -0700

    update layout

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -7,17 +7,13 @@
 
 get_header();
 ?>
+
    <div class="container">
-     <div class="row header-nav">
+     <div class="col-xs-3">
        <h1>test</h1>
      </div>
-     <div class="row">
-       <div class="col-xs-3">
-         <h1>test</h1>
-       </div>
-       <div class="col-xs-9">
-         <h1>test</h1>
-       </div>
+     <div class="col-xs-9">
+       <h1>test</h1>
      </div>
    </div>
 
