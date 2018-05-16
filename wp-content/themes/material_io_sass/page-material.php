b011117212ed26a3cf669788dfeeaab7667b81ee
Sky <wilson.skylerm@gmail.com>
2018-05-15 19:38:34 -0700

    0

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -64,7 +64,7 @@ get_header();
             <div class="mdc-layout-grid__cell--span-6-desktop" style="border:solid black 1px;">
               <h1>Test2</h1>
             </div>
-            <div class="mdc-layout-grid__cell--span-2-desktop mdc-layout-grid-__cell--align-middle" style="border:solid black 1px;">
+            <div class="mdc-layout-grid__cell--span-2-desktop mdc-layout-grid__cell--align-middle" style="border:solid black 1px;">
               <h1>Test3</h1>
             </div>
           </div>
