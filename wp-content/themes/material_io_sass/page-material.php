27a0f5e753072b15faefc70b5a1465cf38c36d8c
Sky <wilson.skylerm@gmail.com>
2018-05-15 19:52:54 -0700

    0

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -44,28 +44,13 @@ get_header();
         <div class="mdc-layout-grid" style="border:solid red 3px;">
           <div class="mdc-layout-grid__inner" style="border:solid green 2px;">
             <div class="mdc-layout-grid__cell--span-6-desktop" style="border:solid black 1px;">
-              <div class="mdc-card">
-              <div class="mdc-card__media mdc-card__media--square">
-              <div class="mdc-card__media-content">Title</div>
-              </div>
-              <!-- ... content ... -->
-              <div class="mdc-card__actions">
-              <div class="mdc-card__action-buttons">
-              <button class="mdc-button mdc-card__action mdc-card__action--button">Action 1</button>
-              <button class="mdc-button mdc-card__action mdc-card__action--button">Action 2</button>
-              </div>
-              <div class="mdc-card__action-icons">
-              <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="Share">share</i>
-              <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="More options">more_vert</i>
-              </div>
-              </div>
-              </div>
+              span 6
             </div>
             <div class="mdc-layout-grid__cell--span-6-desktop" style="border:solid black 1px;">
-              <h1>Test2</h1>
+              span 6
             </div>
-            <div class="mdc-layout-grid__cell--span-2-desktop" style="border:solid black 1px;">
-              <h1>Test3</h1>
+            <div class="mdc-layout-grid__cell--span-2 mdc-layout-grid__cell--order-8" style="border:solid black 1px;">
+              span 4
             </div>
           </div>
         </div>
