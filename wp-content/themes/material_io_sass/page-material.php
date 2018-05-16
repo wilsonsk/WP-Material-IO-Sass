428f79b695c21ecbe596616be816d9f79b2f16ed
Sky <wilson.skylerm@gmail.com>
2018-05-15 19:46:26 -0700

    0

--- wp-content/themes/material_io_sass/page-material.php
+++ wp-content/themes/material_io_sass/page-material.php
@@ -39,27 +39,27 @@ get_header();
     </nav>
 
     <div class="ui__content">
-        <div class="mdc-card">
-        <div class="mdc-card__media mdc-card__media--square">
-        <div class="mdc-card__media-content">Title</div>
-        </div>
-        <!-- ... content ... -->
-        <div class="mdc-card__actions">
-        <div class="mdc-card__action-buttons">
-        <button class="mdc-button mdc-card__action mdc-card__action--button">Action 1</button>
-        <button class="mdc-button mdc-card__action mdc-card__action--button">Action 2</button>
-        </div>
-        <div class="mdc-card__action-icons">
-        <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="Share">share</i>
-        <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="More options">more_vert</i>
-        </div>
-        </div>
-        </div>
 
-        <div class="mdc-layout-grid mdc-layout-grid--align-right" style="border:solid red 3px;">
+
+        <div class="mdc-layout-grid" style="border:solid red 3px;">
           <div class="mdc-layout-grid__inner" style="border:solid green 2px;">
             <div class="mdc-layout-grid__cell--span-6-desktop" style="border:solid black 1px;">
-              <h1>Test1</h1>
+              <div class="mdc-card">
+              <div class="mdc-card__media mdc-card__media--square">
+              <div class="mdc-card__media-content">Title</div>
+              </div>
+              <!-- ... content ... -->
+              <div class="mdc-card__actions">
+              <div class="mdc-card__action-buttons">
+              <button class="mdc-button mdc-card__action mdc-card__action--button">Action 1</button>
+              <button class="mdc-button mdc-card__action mdc-card__action--button">Action 2</button>
+              </div>
+              <div class="mdc-card__action-icons">
+              <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="Share">share</i>
+              <i class="material-icons mdc-card__action mdc-card__action--icon" tabindex="0" role="button" title="More options">more_vert</i>
+              </div>
+              </div>
+              </div>
             </div>
             <div class="mdc-layout-grid__cell--span-6-desktop" style="border:solid black 1px;">
               <h1>Test2</h1>
